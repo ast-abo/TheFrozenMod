ae438e9352d0d0dc2a394b4fa318bedb5b09166b
Andrew <abriano600@gmail.com>
2025-04-17 21:24:42 -0500

    first item!

--- src/main/java/iceman/thefrozenmod/thefrozenmod.java
+++ src/main/java/iceman/thefrozenmod/thefrozenmod.java
@@ -8,7 +8,7 @@ public class thefrozenmod implements ModInitializer {
 
     public static final String MOD_ID = "thefrozenmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+//
     @Override
     public void onInitialize() {
         modItems.initialize();
