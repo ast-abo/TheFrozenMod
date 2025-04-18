b99576c1de63e42198903061ce73b3b52aa84819
Andrew <abriano600@gmail.com>
2025-04-18 18:46:34 -0500

    Abstracted food and effect components

--- src/main/java/iceman/thefrozenmod/modItems.java
+++ src/main/java/iceman/thefrozenmod/modItems.java
@@ -8,19 +8,23 @@ import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ToolMaterial;
 import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Function;
 
 public class modItems {
+    // Items
     public static Item register(String name, @NotNull Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
         RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(thefrozenmod.MOD_ID, name));
         
@@ -32,28 +36,34 @@ public class modItems {
         return item;
     }
 
-    private static ConsumableComponent create(StatusEffect Effect) {
+    private static ConsumableComponent ConstructConsumableComponent(RegistryEntry<StatusEffect> Effect) {
         return ConsumableComponents.food()
-                .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance((RegistryEntry<StatusEffect>) Effect, 6 * 20, 1), 1.0f))
+                .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(Effect, 6 * 20, 1), 1.0f))
                 .build();
     }
 
-    public static final FoodComponent POISON_FOOD_COMPONENT = new FoodComponent.Builder()
-            .alwaysEdible()
-            .build();
+    private static FoodComponent ConstructFoodComponent() {
+        return new FoodComponent.Builder()
+                .alwaysEdible()
+                .build();
+    }
+
+    private static void AddItemToGroup(RegistryKey<ItemGroup> ItemGroup, Item modItem) {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroup)
+                .register((itemGroup) -> itemGroup.add(modItem));
+    }
 
     public static final Item SUSPICIOUS_SUBSTANCE = register(
             "suspicious_substance",
             Item::new,
-            new Item.Settings().food(POISON_FOOD_COMPONENT, create((StatusEffect) StatusEffects.POISON)
+            new Item.Settings().food(ConstructFoodComponent(), ConstructConsumableComponent((RegistryEntry<StatusEffect>) StatusEffects.BLINDNESS))
     );
+
+    // Items
 
 
     public static void initialize() {
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
-                .register((itemGroup) -> itemGroup.add(modItems.SUSPICIOUS_SUBSTANCE));
-
-
+        AddItemToGroup(ItemGroups.INGREDIENTS, modItems.SUSPICIOUS_SUBSTANCE);
     }
 }
 
