93e826a3d8920826cec5363c1a536c5967e2a238
Andrew <abriano600@gmail.com>
2025-04-17 21:23:09 -0500

    first item!

--- src/client/java/iceman/thefrozenmod/client/thefrozenmodClient.java
+++ src/client/java/iceman/thefrozenmod/client/thefrozenmodClient.java
@@ -1,11 +1,19 @@
 package iceman.thefrozenmod.client;
 
+import iceman.thefrozenmod.modItems;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.minecraft.text.Text;
 
 public class thefrozenmodClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
+        ItemTooltipCallback.EVENT.register((itemStack, tooltipContext, tooltipType, list) -> {
+            if (!itemStack.isOf(modItems.SUSPICIOUS_SUBSTANCE)) {
+                return;
+            }
+            list.add(Text.translatable("item.thefrozenmod.suspicious_substance.tooltip"));
+        });
     }
 }
--- src/main/java/iceman/thefrozenmod/modItems.java
+++ src/main/java/iceman/thefrozenmod/modItems.java
@@ -1,18 +1,25 @@
 package iceman.thefrozenmod;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.type.ConsumableComponent;
+import net.minecraft.component.type.ConsumableComponents;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Function;
 
 public class modItems {
-    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+    public static Item register(String name, @NotNull Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
         RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(thefrozenmod.MOD_ID, name));
         
         Item item = itemFactory.apply(settings.registryKey(itemKey));
@@ -23,11 +30,26 @@ public class modItems {
         return item;
     }
 
-    public static final Item SUSPICIOUS_SUBSTANCE = register("suspicious_substance", Item::new, new Item.Settings());
+    public static final ConsumableComponent POISON_FOOD_CONSUMABLE_COMPONENT = ConsumableComponents.food()
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.POISON, 6 * 20, 1), 1.0f))
+            .build();
+
+    public static final FoodComponent POISON_FOOD_COMPONENT = new FoodComponent.Builder()
+            .alwaysEdible()
+            .build();
+
+    public static final Item SUSPICIOUS_SUBSTANCE = register(
+            "suspicious_substance",
+            Item::new,
+            new Item.Settings().food(POISON_FOOD_COMPONENT, POISON_FOOD_CONSUMABLE_COMPONENT)
+    );
+
 
     public static void initialize() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
                 .register((itemGroup) -> itemGroup.add(modItems.SUSPICIOUS_SUBSTANCE));
+
+
     }
 }
 
