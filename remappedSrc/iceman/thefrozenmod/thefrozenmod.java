65cb4dfb755e0969166e5c4299d8961e0c3b9b6b
Andrew <abriano600@gmail.com>
2025-04-18 19:43:45 -0500

    updated to be compatible with kaupenjoe's tutorial

--- remappedSrc/iceman/thefrozenmod/thefrozenmod.java
+++ remappedSrc/iceman/thefrozenmod/thefrozenmod.java
@@ -5,13 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class thefrozenmod implements ModInitializer {
+
     public static final String MOD_ID = "thefrozenmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 
     @Override
     public void onInitialize() {
-
+        modItems.initialize();
     }
 }
  
